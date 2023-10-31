42b0d1b788713e2c9bfaa0bfd004ff564e505bf5
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-10-31 08:17:52 +0600

    (Basic) Area of a Triangle

---
+++ triangleArea.cpp
@@ -0,0 +1,21 @@
+//Basic Area Calculation, here for Triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double base, height, area;
+    cout<<"\t Calculate the Area of a Triangle"<<endl;
+
+    cout<<"Enter the Base: "<<endl;
+    cin>> base;
+
+    cout<<"Enter the Height: "<<endl;
+    cin>> height;
+
+    area = 0.5*base*height;
+    cout<<"Area is "<<area<<endl;
+
+    return 0;
+}
